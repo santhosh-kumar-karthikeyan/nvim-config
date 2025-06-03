41823ed2b3692abe7bb55c297b75018fe7e6915e
Santhosh Kumar Karthikeyan <dhanamsanthosh2005@gmail.com>
2025-06-03 17:12:15 +0000

    add telescope plugin for fuzzy finding across files with fd-find for live grep

--- init.lua
+++ init.lua
@@ -1,1 +1,7 @@
+vim.g.mapleader = " "
 require("config.lazy")
+require("plugins.catppuccin")
+local builtin = require("telescope.builtin")
+vim.keymap.set('n','<C-p>', builtin.find_files, {})
+vim.keymap.set('n','<leader>fg', builtin.live_grep, {})
+vim.cmd.colorscheme "catppuccin"
