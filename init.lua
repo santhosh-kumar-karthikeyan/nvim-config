394d6a28e18fc22b71b4c3621ca5a8af6d1577c1
Santhosh Kumar Karthikeyan <dhanamsanthosh2005@gmail.com>
2025-06-04 14:19:18 +0000

    config treesitter with c, cpp, js, python, lua, md support

--- init.lua
+++ init.lua
@@ -1,6 +1,13 @@
 vim.g.mapleader = " "
 require("config.lazy")
 require("plugins.catppuccin")
+require("plugins.treesitter")
+local tsConfig = require("nvim-treesitter.configs")
+tsConfig.setup({
+	ensure_installed = {"lua","c","cpp","markdown","javascript","python"},
+	highlight = { enable = true },
+	indent = { enable = true }
+})
 local builtin = require("telescope.builtin")
 vim.keymap.set('n','<C-p>', builtin.find_files, {})
 vim.keymap.set('n','<leader>fg', builtin.live_grep, {})
